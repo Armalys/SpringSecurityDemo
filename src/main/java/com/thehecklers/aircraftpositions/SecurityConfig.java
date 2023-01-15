471d280cd4ca5ac7e39f588522b1f5f6d48935cb
Armalys <mashnert@mail.ru>
2023-01-15 23:48:31 +0400

    add roles configurations

--- src/main/java/com/thehecklers/aircraftpositions/SecurityConfig.java
+++ src/main/java/com/thehecklers/aircraftpositions/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.thehecklers.aircraftpositions;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,8 +11,8 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final PasswordEncoder pwEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
     @Bean
@@ -24,12 +26,24 @@ public class SecurityConfig {
         UserDetails jodie = User.builder()
                 .username("jodie")
                 .password(pwEncoder.encode("jpassword"))
-                .roles("USER")
+                .roles("ADMIN")
                 .build();
 
         System.out.println("    >>> Peter`s password: " + peter.getPassword());
         System.out.println("    >>> Jodie`s password: " + jodie.getPassword());
 
         return new InMemoryUserDetailsManager(peter, jodie);
+    }
+
+    // Users authentication
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/aircraftadmin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
     }
 }
